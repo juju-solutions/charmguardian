d44b2bd07945891487d07fcdb847760c1ca193e9
Tim Van Steenburgh <tvansteenburgh@gmail.com>
2014-07-19 13:10:07 -0400

    Add tests for fetcher regex matching

---
+++ charmguardian/tests/test_fetchers.py
@@ -0,0 +1,140 @@
+import unittest
+
+from ..fetchers import (
+    BzrFetcher,
+    BzrMergeProposalFetcher,
+    GithubFetcher,
+    BitbucketFetcher,
+    LocalFetcher,
+)
+
+
+class BzrFetcherTest(unittest.TestCase):
+    def test_can_fetch(self):
+        f = BzrFetcher.can_fetch
+
+        good_tests = [
+            f('lp:~tvansteenburgh/charms/precise/foo'),
+            f('launchpad:~tvansteenburgh/charms/precise/foo'),
+            f('http://launchpad.net/~tvansteenburgh/charms/precise/foo'),
+            f('https://launchpad.net/~tvansteenburgh/charms/precise/foo'),
+            f('http://www.launchpad.net/~tvansteenburgh/charms/precise/foo'),
+            f('https://www.launchpad.net/~tvansteenburgh/charms/precise/foo'),
+            f('http://code.launchpad.net/~tvansteenburgh/charms/precise/foo'),
+            f('https://code.launchpad.net/~tvansteenburgh/charms/precise/foo'),
+        ]
+
+        bad_tests = [
+            f('lp:~tvansteenburgh/charms/precise/foo/+merge/12345'),
+            f('gh:charms/meteor'),
+            f('bb:charms/meteor'),
+            f('local:~/src/charms/precise/meteor'),
+        ]
+
+        for test in good_tests:
+            self.assertEqual(
+                test['repo'],
+                '~tvansteenburgh/charms/precise/foo')
+
+        for test in bad_tests:
+            self.assertEqual(test, {})
+
+
+class BzrMergeProposalFetcherTest(unittest.TestCase):
+    def test_can_fetch(self):
+        f = BzrMergeProposalFetcher.can_fetch
+
+        good_tests = [
+            f('lp:~tvansteenburgh/charms/precise/foo/+merge/12345'),
+        ]
+
+        bad_tests = [
+            f('lp:~tvansteenburgh/charms/precise/foo'),
+            f('gh:charms/meteor'),
+            f('bb:charms/meteor'),
+            f('local:~/src/charms/precise/meteor'),
+        ]
+
+        for test in good_tests:
+            self.assertEqual(
+                test['repo'],
+                '~tvansteenburgh/charms/precise/foo/+merge/12345')
+
+        for test in bad_tests:
+            self.assertEqual(test, {})
+
+
+class GithubFetcherTest(unittest.TestCase):
+    def test_can_fetch(self):
+        f = GithubFetcher.can_fetch
+
+        good_tests = [
+            f('gh:charms/meteor'),
+            f('github:charms/meteor'),
+            f('http://github.com/charms/meteor'),
+            f('https://github.com/charms/meteor'),
+            f('http://www.github.com/charms/meteor'),
+            f('https://www.github.com/charms/meteor'),
+        ]
+
+        bad_tests = [
+            f('lp:~tvansteenburgh/charms/precise/foo'),
+            f('lp:~tvansteenburgh/charms/precise/foo/+merge/12345'),
+            f('bb:charms/meteor'),
+            f('local:~/src/charms/precise/meteor'),
+        ]
+
+        for test in good_tests:
+            self.assertEqual(test['repo'], 'charms/meteor')
+
+        for test in bad_tests:
+            self.assertEqual(test, {})
+
+
+class BitbucketFetcherTest(unittest.TestCase):
+    def test_can_fetch(self):
+        f = BitbucketFetcher.can_fetch
+
+        good_tests = [
+            f('bb:charms/meteor'),
+            f('bitbucket:charms/meteor'),
+            f('http://bitbucket.org/charms/meteor'),
+            f('https://bitbucket.org/charms/meteor'),
+            f('http://www.bitbucket.org/charms/meteor'),
+            f('https://www.bitbucket.org/charms/meteor'),
+        ]
+
+        bad_tests = [
+            f('lp:~tvansteenburgh/charms/precise/foo'),
+            f('lp:~tvansteenburgh/charms/precise/foo/+merge/12345'),
+            f('gh:charms/meteor'),
+            f('local:~/src/charms/precise/meteor'),
+        ]
+
+        for test in good_tests:
+            self.assertEqual(test['repo'], 'charms/meteor')
+
+        for test in bad_tests:
+            self.assertEqual(test, {})
+
+
+class LocalFetcherTest(unittest.TestCase):
+    def test_can_fetch(self):
+        f = LocalFetcher.can_fetch
+
+        good_tests = [
+            f('local:~/src/charms/precise/meteor'),
+        ]
+
+        bad_tests = [
+            f('lp:~tvansteenburgh/charms/precise/foo'),
+            f('lp:~tvansteenburgh/charms/precise/foo/+merge/12345'),
+            f('gh:charms/meteor'),
+            f('bb:charms/meteor'),
+        ]
+
+        for test in good_tests:
+            self.assertEqual(test['path'], '~/src/charms/precise/meteor')
+
+        for test in bad_tests:
+            self.assertEqual(test, {})
